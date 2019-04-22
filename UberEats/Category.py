5526d1de494609d93d8a4a0b4f3eb1102f3106cd
dipalpatel77 <pateldipal47@gmail.com>
2019-04-22 14:39:52 -0400

    added new claas

--- UberEats/Category.py
+++ UberEats/Category.py
@@ -1,1 +1,9 @@
 import datetime
+class Category(object):
+    """docstring for Category."""
+
+    def __init__(self, newName, newDate):
+        super(Category, self).__init__()
+        x = datetime.datetime.now()
+        print (x)
+        self.arg = arg
