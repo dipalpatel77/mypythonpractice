7f7e437bf6329ebde1df66dbc52e89c2dbb55d88
dipalpatel77 <pateldipal47@gmail.com>
2019-05-17 14:11:51 -0400

    class and banana example from hackerrank

---
+++ aas.py
@@ -0,0 +1,5 @@
+b = 0
+while (b < 6):
+    print(b)
+    b = b + 1
+
--- test_deluxePizza.py
+++ test_deluxePizza.py
@@ -1,5 +1,13 @@
 from unittest import TestCase
+#import DeluxePizza
 
 
 class TestDeluxePizza(TestCase):
-    pass
+    cheese_topping = input(" enter the six input ")
+    pepproni_topping = input(" enter the six input ")
+    mushroom_topping = input(" enter the six input ")
+    veggie_topping = input(" enter the six input ")
+    number_of_pizza = input(" enter the six input ")
+    stuffed_with_cheese = input(" enter the six input ")
+    dp = DeluxePizza(cheese_topping,pepproni_topping,mushroom_topping,veggie_topping,stuffed_with_cheese)
+
