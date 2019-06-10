791838029bb252445bc6b6dfc6463c42ae1756aa
dipal <pateldipal47@gmail.com>
2019-06-10 16:13:38 -0400

    allstaged

--- list/avgmarks.py
+++ list/avgmarks.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        line = raw_input().split()
+        line = input().split()
         name, scores = line[0], line[1:]
         scores = map(float, scores)
         student_marks[name] = scores
-    query_name = raw_input()
+    query_name = input("name = ")
     if query_name in student_marks:
-    	print "%.2f" % sum(student_marks[query_name])/len(student_marks[query_name])
+        print ("%.2f" % sum(student_marks[query_name])/len(student_marks[query_name]))
--- re.py
+++ re.py
@@ -5,7 +5,7 @@ value = "one 1 two 2 three 3"
 
 # Separate on one or more non-digit characters.
 result = re.split("\D+", value)
-print type(result)
+print (type(result))
 # Print results.
 
 for element in result:
@@ -14,4 +14,4 @@ for element in result:
 a= [12,21]
 b=[12,32]
 
-print list(product(a,b))
+print (list(product(a,b)))
