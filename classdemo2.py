12d317cd97c5ec92c092f23800a02e44af741f15
dipal <pateldipal47@gmail.com>
2020-03-24 11:33:55 -0400

    checksum

--- classdemo2.py
+++ classdemo2.py
@@ -3,18 +3,18 @@ class inside:
     x=388
     y=3298
     def __init__(self):
-        print"called:",type(self)
+        print ("called:",type(self))
        # print(x)
         pass
     def addition(self, a, b):
-        print"sum=",a + b
+        print("sum=",a + b)
     def subtracton(self, a, b):
-        print "sub=",a - b
+        print ("sub=",a - b)
         return 0
     def main(self):
       a = int(input("enter the value of a"))
       b = int(input("enter the value of b"))
-      print " ".join(raw_input()) 
+      print (" ".join(input()))
       ic=inside()
       print(ic.addition(a, b))
       print(ic.subtracton(a, b))
---
+++ list/luhn.py
@@ -0,0 +1,26 @@
+def checknum(self):
+    isodd = False
+    a = list(map(int,self))
+    nsum = 0
+    for i in range(len(a)):
+        if(isodd):
+            a[i] = a[i] * 2
+        isodd = ~ (isodd)
+        nsum += int( a[i] / 10)
+        nsum += int( a[i] % 10)
+    if(nsum % 10 == 0):
+        print("the is valid")
+    else:
+        print("it's not valid")
+
+c = True
+while(c):
+    a = input("enter numbers")
+    b = list(map(int,a))
+    #b.reverse()
+    checknum(b)
+    d = input("do you want to continue 'y' for yes 'n' for no : ")
+    if(d == 'y'):
+        c = True
+    else:
+        c = False
